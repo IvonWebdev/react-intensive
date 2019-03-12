c84d4e533075dc9a84f42612ec3ebadff2e664cf
Ivon <ivon.webdev@gmail.com>
2019-03-13 00:41:59 +0200

    First lesson. Step 1

---
+++ source/components/Composer/index.js
@@ -0,0 +1,15 @@
+// Core
+import React, { Component } from 'react';
+
+// Instruments
+import avatar from 'theme/assets/lisa';
+
+export default class Composer extends Component {
+    render() {
+        return (
+            <section>
+                <img src = { avatar } />
+            </section> 
+        );
+    }
+}
