97c407043ecc4896087ff5c5db0c4ced58a23209
Ivon <ivon.webdev@gmail.com>
2019-04-16 20:21:02 +0300

    Homework 10

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,6 @@
 // Core
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 // Components
@@ -171,13 +171,23 @@ class Feed extends Component {
 
         const postJSQX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post
-                        { ...post }
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost }
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = {{
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                        exit:        Styles.postOutStart,
+                        exitActive:  Styles.postOutEnd,
+                    }}
+                    key = { post.id }
+                    timeout = {{ enter: 500, exit: 500 }}>
+                    <Catcher>
+                        <Post
+                            { ...post }
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost }
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -200,8 +210,7 @@ class Feed extends Component {
                     onEntered = { this._animatePostmanLeave }>
                     <Postman />
                 </Transition>
-
-                {postJSQX}
+                <TransitionGroup>{postJSQX}</TransitionGroup>
             </section>
         );
     }
